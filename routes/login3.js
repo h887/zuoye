281508349dc4ee87d6ab0675d1858379aecf2dc1
对你可真服 <1548538076@qq.com>
2020-11-07 16:22:25 +0800

    node

---
+++ routes/login3.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+const user = require("../sql/user");
+
+
+router.get("/", function (req, res, next) {
+    console.log('此时进入了login3')
+      res.render("login3");
+    });
+
+
+    router.post("/in", function (req, res, next) {
+        // console.log("进入login3 的in 处理");
+      
+        let obj = req.body;
+        // console.log(obj)
+        // console.log(obj.username);
+        // console.log(obj.password)
+        //这一步是查询里面有没有 用户输入的 username password 有就可以跳后面页面 没有不可以
+        user.findOne({username: req.body.username, password: req.body.password}, (err, data) => {
+          if (err) {
+            console.log(err);
+          }
+          if(data) {
+            req.session.islogin = 'ok'
+              res.redirect('/pro')
+          } else {
+              res.redirect('/register3')
+          }
+         
+           
+        });
+      });
+      
+module.exports = router;
---
+++ routes/register3.js
@@ -0,0 +1,53 @@
+var express = require("express");
+//建立一个路由空表！！！！
+var router = express.Router();
+//引入user 模型 类似于英雄联盟  只能有六神装的设计
+const user = require("../sql/user");
+
+
+router.get("/", function (req, res, next) {
+    console.log('此时进入了register3')
+      res.render("register");
+    });
+    router.post("/in", function (req, res, next) {
+        console.log("进入register3 的in 处理");
+      
+        let obj = req.body;
+        console.log(obj)
+        console.log(obj.username);
+        console.log(obj.password)
+        
+        user.insertMany(obj,(err,data)=>{
+            if(err) {
+                console.log(err)
+            }
+            console.log(data)
+
+            if(data) {
+                res.redirect('/login3')
+            }else {
+                res.redirect('/register3')
+            }
+
+        })
+
+        // 解决重复注册问题
+        user.findOne({ username: obj.username }, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            if (data) {
+                res.redirect('/register3')
+            } else {
+                user.insertMany(obj, (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(data);
+                    res.redirect('./login3')
+                })
+            }
+        })
+      });
+
+module.exports = router;
